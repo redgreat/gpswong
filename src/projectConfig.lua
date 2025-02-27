aac7c318155869449fc0b206e3629543201dcf36
wongcw <rubygreat@msn.com>
2025-02-27 13:50:54 +0800

    config update

--- src/projectConfig.lua
+++ src/projectConfig.lua
@@ -3,12 +3,12 @@
 -- ③自建服务器(cloudwong)： 12个参数--必配--自行在自建服务器处理
 _G.SRCCID = mobile.imei()
 _G.mqtt_mqttClientId = mobile.imei()
-_G.mqtt_username = "admin"
-_G.mqtt_passwd = "123456"
-_G.mqtt_mqttHostUrl = "127.0.0.1"
+_G.mqtt_username = "aaa"
+_G.mqtt_passwd = "bbb"
+_G.mqtt_mqttHostUrl = "ccc"
 _G.mqtt_port = 1883
-_G.mqtt_pub_topic = "Pub/780eg/" .. mobile.imei() .. "/data"
-_G.mqtt_sub_topic = "Sub/780eg/" .. mobile.imei() .. "/cmd"
+_G.mqtt_pub_topic = "pub/780eg/" .. mobile.imei() .. "/data"
+_G.mqtt_sub_topic = "sub/780eg/" .. mobile.imei() .. "/cmd"
 
 ------------------------------以上必配，到此结束--------------------------------------
 
@@ -24,25 +24,25 @@ _G.PositioningMode="$PCAS04,3*1A\r\n"      --GPS+北斗双模定位
 _G.logFlag = false;
 
 -- 设备运行模式 ，默认正常： 正常awake_normal  深度休眠restdeep_deviceupdate  主动查询restdeep_platequery
-_G.devicemodel = "";
+_G.devicemodel = "restdeep_deviceupdate";
 
 -- 扩展命令 用于回复指令 platformquery
 _G.cmd_ext = "";
 
--- 数据上报周期--默认的15S上报:15*1000ms
-_G.update_time = 15*1000
+-- 数据上报周期--默认的10S上报:10*1000ms
+_G.update_time = 10*1000
 
--- 深度休眠唤醒周期 10分钟
-_G.deeprest_time = 10*60*1000
+-- 深度休眠唤醒周期 1分钟
+_G.deeprest_time = 1*60*1000
 
 -- 消息传输模板
-_G.REPORT_DATA_TEMPLATE = "{\"reporttime\":\"\", \"electricity\":\"--\"}"
+_G.REPORT_DATA_TEMPLATE = "{\"reporttime\":\"\"}"
 _G.REPORT_CONTROLLACK_TEMPLATE = "{\"status\":\"success\",\"cmdtype\":\"cmd_controllack\",\"did\":\"\",\"reporttime\":\"\"}"
 
 -- 默认的经纬度和数据源
 _G.old_latitude=""
 _G.old_longitude=""
-_G.data_from="NoData"
+_G.data_from=""
 
 -- 默认的温湿度
 _G.temperature=0
@@ -67,3 +67,6 @@ _G.SatsNum=0
 _G.GPS_Ggt_Topic="GPS_Get"
 _G.GPS_Ggt_Topic_F="GPS_Get_F"
 _G.Updata_OK="Updata_OK"
+
+-- LBS定位
+_G.PRODUCT_KEY = "zqv3DfiOoIx0waquuK9LyuoHiflBXDpS"
